5e9738675cb66578e01a0f837511035d67054212
Alper Konuralp <alper@konuralp.gen.tr>
2010-06-27 15:53:09 +0300

    15:53

---
+++ NetBlog.Controller/DataSources/BlogDataSource.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NetBlog.Controller.DataSources
+{
+    public class BlogDataSource : System.Web.UI.DataSourceControl
+    {
+        protected override System.Web.UI.DataSourceView GetView(string viewName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
